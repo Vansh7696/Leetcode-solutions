72ba5cff523e7d9b9369285e88af5fba43c965ba
Vansh7696 <studiousvansh@gmail.com>
2026-08-24 18:43:33 +0530

    Time: 4 ms (99.05%), Space: 47.7 MB (68.85%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -3,9 +3,7 @@ class Solution {
         int i=0;
         int j=s.length()-1;
         while(i<j){
-            char left = s.charAt(i);
-            char right = s.charAt(j);
-            if(left!=right){
+            if(s.charAt(i)!=s.charAt(j)){
                 return helper(i+1,j,s) || helper(i,j-1,s);
             }else{
                 i++;
@@ -14,11 +12,9 @@ class Solution {
         }
         return true;
     }
-    public boolean helper(int i,int j, String s){
+    public boolean helper(int i,int j,String s){
         while(i<j){
-            char left = s.charAt(i);
-            char right = s.charAt(j);
-            if(Character.toLowerCase(left)!=Character.toLowerCase(right)){
+            if(s.charAt(i)!=s.charAt(j)){
                 return false;
             }
             i++;
